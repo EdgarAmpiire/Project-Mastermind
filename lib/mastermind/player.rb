aa627197c4066fa1852e59f96f37505a20ee617e
Edgar Ampiire <eampiire@gmail.com.com>
2025-07-13 09:41:37 +0300

    updated player class with functionality to choose code

--- lib/mastermind/player.rb
+++ lib/mastermind/player.rb
@@ -7,4 +7,13 @@ class Player
       puts "Invalid guess. Use 4 letters from #{Code::COLORS.join(', ')}"
     end
   end
+
+  def set_secret_code
+    loop do
+      print 'Enter your secret code (e.g. RGBY):'
+      input = gets.chomp.upcase.chars
+      return input if Code.valid_guess?(input)
+      puts "Invalid code. Use 4 letters from #{Code::COLORS.join(', ')}"
+    end
+  end
 end
